b4896a0224d4f47a343f8e172a46c32849678a40
Olga Li <leka.ru97@mail.ru>
2021-11-21 00:41:56 +0300

    install all packages

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import Koa from 'koa'
 const app = new Koa()
 
 app.use(async (ctx: any) => {
-  ctx.body = {str: 123}
+  ctx.body = "123"
   // await next()
 })
 
-app.listen(3002)
+app.listen(3000)
