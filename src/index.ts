7127e8a6fe22b1ad0e30a2097a62068325e16f96
John Doe <dsshard@gmail.com>
2021-11-28 22:19:33 +0300

    28 nov

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
 import Koa from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors';
+
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async (ctx: any) => {
-  ctx.body = "123"
-  // await next()
+app.use(bodyParser());
+app.use(cors());
+
+
+const user = {
+  name: 'Petya',
+  lastname: 'Petrov'
+}
+router.get('/api/users', async (ctx, next) => {
+  ctx.body = [user, user, user]
+  await next()
 })
 
-app.listen(3000)
+router.get('/api/user', async (ctx, next) => {
+  ctx.body = user
+  await next()
+})
+
+router.post('/api/login', async (ctx, next) => {
+  console.log('attempt login', ctx.request.body)
+  if (ctx.request.body.login === 'petya' && String(ctx.request.body.password) === '123') {
+    ctx.body = 'petya'
+    return
+  }
+
+  if (ctx.request.body.login === 'vasya' && String(ctx.request.body.password) === '123') {
+    ctx.body = 'vasya'
+    return
+  }
+
+  ctx.body = 'failed'
+  await next()
+})
+
+app.use(router.middleware())
+
+
+app.listen(3001)
