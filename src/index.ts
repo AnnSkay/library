f259ca902c80a478efd25132bcfec9fb4b55c7cb
Olga Li <leka.ru97@mail.ru>
2022-01-04 15:01:30 +0300

    личный кабинет

--- src/index.ts
+++ src/index.ts
@@ -41,28 +41,38 @@ const genres = [
   {
     genre: 'Комедия',
   },
-  {genre: 'Сказки',
+  {
+    genre: 'Сказки',
   },
 ]
 
-const users = [
+let users = [
   {
+    id: 111,
     name: 'Петя',
     lastname: 'Петров',
     login: 'petya@mail.ru',
     password: '123',
+    role: 'USER',
+    phone: '89991112233',
   },
   {
+    id: 222,
     name: 'Вася',
     lastname: 'Васильев',
     login: 'vasya@mail.ru',
     password: '123',
+    role: 'ADMIN',
+    phone: '89112345692',
   },
   {
+    id: 333,
     name: 'Федя',
     lastname: 'Федоров',
     login: 'fedya@mail.ru',
     password: '123',
+    role: 'LIBR',
+    phone: '89623335823',
   }
 ]
 
@@ -179,7 +189,7 @@ router.get(`/api/genres`, async (ctx, next) => {
 })
 
 router.get(`/api/houses`, async (ctx, next) => {
-  console.log('attempt genres', ctx.request.body)
+  console.log('attempt houses', ctx.request.body)
   ctx.body = houses
   await next()
 })
@@ -193,27 +203,66 @@ router.get('/api/users', async (ctx, next) => {
 router.post(`/api/user`, async (ctx, next) => {
   console.log('attempt user', ctx.request.body)
   for (let i = 0; i < users.length; i++) {
-    if (ctx.request.body.name === users[i].name) {
-      ctx.body = users[i].lastname
+    if (users[i].id === Number(ctx.request.body.id)) {
+      if (users[i].name === '') {
+        ctx.body = 'Читатель'
+        return
+      } else {
+        ctx.body = users[i]
+        return
+      }
+    }
+  }
+
+  ctx.body = 'this user was not found'
+  await next()
+})
+
+router.post(`/api/changeUserData`, async (ctx, next) => {
+  console.log('attempt user', ctx.request.body)
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === Number(ctx.request.body.userId)) {
+      users[i].name = ctx.request.body.name
+      users[i].lastname = ctx.request.body.lastname
+      users[i].login = ctx.request.body.email
+      users[i].phone = ctx.request.body.phone
+
+      ctx.body = users[i]
       return
     }
   }
+
+  ctx.body = 'this user was not found'
+  await next()
+})
+
+router.post(`/api/changeUserPassword`, async (ctx, next) => {
+  console.log('attempt user', ctx.request.body)
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id === Number(ctx.request.body.userId)) {
+      users[i].password = ctx.request.body.newPassword
+
+      ctx.body = users[i]
+      return
+    }
+  }
+
   ctx.body = 'this user was not found'
   await next()
 })
 
 router.post(`/api/books`, async (ctx, next) => {
   console.log('attempt books', ctx.request.body)
-  let booksFilter: any[];
-  booksFilter = [];
+  let booksFilter: any[]
+  booksFilter = []
 
   for (let i = 0; i < books.length; i++) {
-    if (books[i].title.includes(ctx.request.body.bookTitle) &&
-        books[i].author.includes(ctx.request.body.bookAuthor) &&
-        books[i].house.includes(ctx.request.body.bookPublishHouse) &&
-        books[i].genre.includes(ctx.request.body.bookGenre) &&
-        (books[i].year === Number(ctx.request.body.bookPublishYear) ||  ctx.request.body.bookPublishYear === '') &&
-        (books[i].isAvailable === ctx.request.body.bookIsAvailable && ctx.request.body.bookIsAvailable === true || ctx.request.body.bookIsAvailable === false)
+    if (books[i].title.includes(ctx.request.body.title) &&
+        books[i].author.includes(ctx.request.body.author) &&
+        books[i].house.includes(ctx.request.body.publishHouse) &&
+        books[i].genre.includes(ctx.request.body.genre) &&
+        (books[i].year === Number(ctx.request.body.publishYear) || ctx.request.body.publishYear === '') &&
+        (books[i].isAvailable === ctx.request.body.isAvailable && ctx.request.body.isAvailable === true || ctx.request.body.isAvailable === false)
        )
     {
       booksFilter.push(books[i])
@@ -221,7 +270,6 @@ router.post(`/api/books`, async (ctx, next) => {
   }
 
   ctx.body = booksFilter
-
   await next()
 })
 
@@ -230,7 +278,7 @@ router.post('/api/login', async (ctx, next) => {
   for (let i = 0; i < users.length; i++) {
     if (ctx.request.body.login === users[i].login &&
         ctx.request.body.password === users[i].password) {
-          ctx.body = users[i].login
+          ctx.body = users[i].id
           return
     }
   }
