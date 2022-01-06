cad8855321a83b437488e550d19cbff50ea25ede
Olga Li <leka.ru97@mail.ru>
2022-01-06 16:01:37 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors';
-import { DataType } from 'sequelize-typescript';
+// import { DataType } from 'sequelize-typescript';
 
 const app = new Koa()
 const router = new Router()
@@ -12,37 +12,47 @@ app.use(cors());
 
 let houses = [
   {
-    house: 'РОСМЭН',
+    id: 1,
+    title: 'РОСМЭН',
   },
   {
-    house: 'Издательство1',
+    id: 2,
+    title: 'Издательство1',
   },
   {
-    house: 'Издательство2',
+    id: 3,
+    title: 'Издательство2',
   },
   {
-    house: 'Издательство3',
+    id: 4,
+    title: 'Издательство3',
   },
   {
-    house: 'Издательство4',
+    id: 5,
+    title: 'Издательство4',
   },
 ];
 
 let genres = [
   {
-    genre: 'Фантастика',
+    id: 1,
+    title: 'Фантастика',
   },
   {
-    genre: 'Фэнтэзи',
+    id: 2,
+    title: 'Фэнтэзи',
   },
   {
-    genre: 'Ужасы'
+    id: 3,
+    title: 'Ужасы'
   },
   {
-    genre: 'Комедия',
+    id: 4,
+    title: 'Комедия',
   },
   {
-    genre: 'Сказки',
+    id: 5,
+    title: 'Сказки',
   },
 ]
 
@@ -81,8 +91,8 @@ let books = [
     id: 1,
     title: 'Онегин',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 2,
     year: 2000,
     numberCopyes: 2,
   },
@@ -90,8 +100,8 @@ let books = [
     id: 2,
     title: 'Война и мир',
     author: 'Толстой',
-    house: 'блабла',
-    genre: 'Фантастика',
+    houseId: 3,
+    genreId: 2,
     year: 1900,
     numberCopyes: 1,
   },
@@ -99,8 +109,8 @@ let books = [
     id: 3,
     title: 'Капитанская дочка',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 2,
+    genreId: 1,
     year: 2000,
     numberCopyes: 0,
   },
@@ -108,8 +118,8 @@ let books = [
     id: 4,
     title: 'Книга1',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 5,
     year: 2000,
     numberCopyes: 1,
   },
@@ -117,8 +127,8 @@ let books = [
     id: 5,
     title: 'Книга2',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 5,
+    genreId: 3,
     year: 2000,
     numberCopyes: 3,
   },
@@ -126,8 +136,8 @@ let books = [
     id: 6,
     title: 'Книга3',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 2,
+    genreId: 2,
     year: 2000,
     numberCopyes: 1,
   },
@@ -135,8 +145,8 @@ let books = [
     id: 7,
     title: 'Книга4',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 2,
+    genreId: 2,
     year: 2000,
     numberCopyes: 1,
   },
@@ -144,8 +154,8 @@ let books = [
     id: 8,
     title: 'Книга5',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 4,
+    genreId: 4,
     year: 2000,
     numberCopyes: 1,
   },
@@ -153,8 +163,8 @@ let books = [
     id: 9,
     title: 'Книга6',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 3,
     year: 2000,
     numberCopyes: 1,
   },
@@ -162,8 +172,8 @@ let books = [
     id: 10,
     title: 'Книга7',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 4,
     year: 2000,
     numberCopyes: 0,
   },
@@ -171,8 +181,8 @@ let books = [
     id: 11,
     title: 'Книга8',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 2,
     year: 2000,
     numberCopyes: 0,
   },
@@ -180,8 +190,8 @@ let books = [
     id: 12,
     title: 'Книга9',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 2,
     year: 2000,
     numberCopyes: 0,
   },
@@ -189,8 +199,8 @@ let books = [
     id: 13,
     title: 'Книга10',
     author: 'Пушкин',
-    house: 'РОСМЭН',
-    genre: 'Фэнтэзи',
+    houseId: 1,
+    genreId: 2,
     year: 2000,
     numberCopyes: 1,
   },
@@ -217,6 +227,7 @@ let borrowedBooks = [
   },
 ]
 
+// отправляет список жанров для раскрывающего списка поиска книг
 router.get('/api/genres', async (ctx, next) => {
   console.log('attempt genres', ctx.request.body)
 
@@ -225,6 +236,7 @@ router.get('/api/genres', async (ctx, next) => {
   await next()
 })
 
+// отправляет список издательств для раскрывающего списка поиска книг
 router.get('/api/houses', async (ctx, next) => {
   console.log('attempt houses', ctx.request.body)
 
@@ -233,20 +245,23 @@ router.get('/api/houses', async (ctx, next) => {
   await next()
 })
 
-router.get('/api/users', async (ctx, next) => {
-  console.log('attempt users', ctx.request.body)
+// получение списка всех пользователей (тестовый запрос, нигде не используется)
+// router.get('/api/users', async (ctx, next) => {
+//   console.log('attempt users', ctx.request.body)
 
-  ctx.body = users
+//   ctx.body = users
 
-  await next()
-})
+//   await next()
+// })
 
+// отправляет данные пользователя по полученному id
 router.post('/api/user', async (ctx, next) => {
   console.log('attempt user', ctx.request.body)
 
   for (let i = 0; i < users.length; i++) {
     if (users[i].id === Number(ctx.request.body.id)) {
       ctx.body = users[i]
+
       return
     }
   }
@@ -256,40 +271,40 @@ router.post('/api/user', async (ctx, next) => {
   await next()
 })
 
+// добавляет пользователя в базу при регистрации по полученным данным из полей ввода
 router.post('/api/addUser', async (ctx, next) => {
-  console.log('attempt user', ctx.request.body)
-
-  let isNew = true
+  console.log('attempt addUser', ctx.request.body)
 
   for (let i = 0; i < users.length; i++) {
     if (users[i].login === ctx.request.body.email) {
       ctx.body = 'Пользователь с таким email уже существует'
-      isNew = false
+
       return
     }
   }
 
-  if (isNew) {
-    let user: any
-    user = {}
+  let user: any
+  user = {}
 
-    user.id = users[users.length - 1].id + 1;
-    user.name = ctx.request.body.name
-    user.lastname = ctx.request.body.lastname
-    user.login = ctx.request.body.email
-    user.password = ctx.request.body.password
-    user.role = 'USER'
-    user.phone = ctx.request.body.phone
+  user.id = users[users.length - 1].id + 1
+  user.name = ctx.request.body.name
+  user.lastname = ctx.request.body.lastname
+  user.login = ctx.request.body.email
+  user.password = ctx.request.body.password
+  user.role = 'USER'
+  user.phone = ctx.request.body.phone
 
-    users.push(user)
-    ctx.body = `Пользователь с ID: ${users[users.length - 1].id} добавлен`
-  }
+  users.push(user)
+
+  ctx.body = `Пользователь с ID: ${users[users.length - 1].id} добавлен`
 
   await next()
 })
 
+// изменяет личные данные пользователя
 router.post('/api/changeUserData', async (ctx, next) => {
-  console.log('attempt user', ctx.request.body)
+  console.log('attempt changeUserData', ctx.request.body)
+
   for (let i = 0; i < users.length; i++) {
     if (users[i].id === Number(ctx.request.body.userId)) {
       users[i].name = ctx.request.body.name
@@ -303,47 +318,94 @@ router.post('/api/changeUserData', async (ctx, next) => {
   }
 
   ctx.body = 'this user was not found'
+
   await next()
 })
 
+// изменяет пароль пользователя
 router.post('/api/changeUserPassword', async (ctx, next) => {
-  console.log('attempt user', ctx.request.body)
+  console.log('attempt changeUserPassword', ctx.request.body)
+
   for (let i = 0; i < users.length; i++) {
     if (users[i].id === Number(ctx.request.body.userId)) {
       users[i].password = ctx.request.body.newPassword
 
       ctx.body = users[i]
+
       return
     }
   }
 
   ctx.body = 'this user was not found'
+
   await next()
 })
 
+// поиск книг по полученному фильтру
 router.post('/api/books', async (ctx, next) => {
   console.log('attempt books', ctx.request.body)
+
   let booksFilter: any[]
   booksFilter = []
+
+  let houseId = -1
+  let genreId = -1
+
+  for (let i = 0; i < houses.length; i++) {
+    if (ctx.request.body.publishHouse !== '') {
+      if (ctx.request.body.publishHouse === houses[i].title) {
+        houseId = houses[i].id
+      }
+    }
+  }
+
+  for (let i = 0; i < genres.length; i++) {
+    if (ctx.request.body.genre !== '') {
+      if (ctx.request.body.genre === genres[i].title) {
+        genreId = genres[i].id
+      }
+    }
+  }
 
   for (let i = 0; i < books.length; i++) {
     if (books[i].title.includes(ctx.request.body.title) &&
         books[i].author.includes(ctx.request.body.author) &&
-        books[i].house.includes(ctx.request.body.publishHouse) &&
-        books[i].genre.includes(ctx.request.body.genre) &&
+        (books[i].houseId === houseId || ctx.request.body.publishHouse === '') &&
+        (books[i].genreId === genreId || ctx.request.body.genre === '') &&
         (books[i].year === Number(ctx.request.body.publishYear) || ctx.request.body.publishYear === '') &&
         ((books[i].numberCopyes > 0 && ctx.request.body.isAvailable === true) || ctx.request.body.isAvailable === false)
     ) {
       booksFilter.push(books[i])
+
+      let houseTitle = ''
+      let genreTitle = ''
+
+      for (let j = 0; j < houses.length; j++) {
+        if (houses[j].id === books[i].houseId) {
+          houseTitle = houses[j].title
+        }
+      }
+
+      for (let j = 0; j < genres.length; j++) {
+        if (genres[j].id === books[i].genreId) {
+          genreTitle = genres[j].title
+        }
+      }
+
+      booksFilter[booksFilter.length - 1].houseTitle = houseTitle
+      booksFilter[booksFilter.length - 1].genreTitle = genreTitle
     }
   }
 
   ctx.body = booksFilter
+
   await next()
 })
 
+// отправляет список книг, которые находятся на руках у пользователя, по полученному id пользователя
 router.post('/api/borrowedBooks', async (ctx, next) => {
   console.log('attempt borrowedBooks', ctx.request.body)
+
   let userBorrowedBooks: any[]
   userBorrowedBooks = []
 
@@ -354,6 +416,7 @@ router.post('/api/borrowedBooks', async (ctx, next) => {
       for (let j = 0; j < books.length; j++) {
         if (books[j].id === borrowedBooks[i].bookId) {
           userBorrowedBooks.push(books[j])
+
           userBorrowedBooks[userBorrowedBooks.length - 1].dateIssue = borrowedBooks[i].dateIssue
         }
       }
@@ -361,11 +424,14 @@ router.post('/api/borrowedBooks', async (ctx, next) => {
   }
 
   ctx.body = userBorrowedBooks
+
   await next()
 })
 
+// позволяет взять книгу пользователю
 router.post('/api/takeBook', async (ctx, next) => {
   console.log('attempt takeBook', ctx.request.body)
+
   let userTakenBook = {
     bookId: 0,
     userId: 0,
@@ -378,6 +444,7 @@ router.post('/api/takeBook', async (ctx, next) => {
         borrowedBooks[i].userId === Number(ctx.request.body.id) &&
         borrowedBooks[i].dateReturn.getTime() > new Date().getTime()) {
           ctx.body = 'Эта книга уже была взята Вами'
+
           return
         }
   }
@@ -388,17 +455,19 @@ router.post('/api/takeBook', async (ctx, next) => {
         }
   }
 
-  userTakenBook.bookId = Number(ctx.request.body.bookId);
-  userTakenBook.userId = Number(ctx.request.body.id);
-  userTakenBook.dateIssue = new Date();
-  userTakenBook.dateReturn =  new Date(2032, 0, 1);
+  userTakenBook.bookId = Number(ctx.request.body.bookId)
+  userTakenBook.userId = Number(ctx.request.body.id)
+  userTakenBook.dateIssue = new Date()
+  userTakenBook.dateReturn =  new Date(2032, 0, 1)
 
-  borrowedBooks.push(userTakenBook);
+  borrowedBooks.push(userTakenBook)
 
   ctx.body = 'Книга взята'
+
   await next()
 })
 
+// позволяет вернуть взятую книгу
 router.post('/api/returnBook', async (ctx, next) => {
   console.log('attempt returnBook', ctx.request.body)
   for (let i = 0; i < borrowedBooks.length; i++) {
@@ -415,9 +484,11 @@ router.post('/api/returnBook', async (ctx, next) => {
   }
 
   ctx.body = borrowedBooks
+
   await next()
 })
 
+// осуществляет вход пользователя по логину и паролю
 router.post('/api/login', async (ctx, next) => {
   console.log('attempt login', ctx.request.body)
   for (let i = 0; i < users.length; i++) {
